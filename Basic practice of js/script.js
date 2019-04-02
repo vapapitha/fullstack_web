991bd779a5f766256f6eaede56588d6706a641ee
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-03 05:12:50 +0600

    Add files via upload

---
+++ Basic practice of js/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt ("What is your first name?");
+var lastName = prompt ("What is your last name?");
+var age = prompt ("What is your age?");
+console.log ("your full name is " + firstName + " " + lastName );
+console.log ("you are " + age +  " " + "years old" );
