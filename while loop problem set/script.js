ca81975bbbab00d4610d6d47edf8aeb8ac00a745
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-06 03:36:43 +0600

    Add files via upload

---
+++ while loop problem set/script.js
@@ -0,0 +1,31 @@
+console.log ("printing all number between -10 and 19")
+var count= -10;
+while (count <20) {
+    console.log (count);
+    count++;
+}
+
+console.log ("printing all number between 10 and 40")
+var count= 10;
+while (count <=40) {
+    console.log (count);
+    count+=2;
+}
+
+
+console.log ("print out all odd number between 300 and 333")
+var count= 300;
+while (count <=333){
+    if( count% 2!==0)
+    console.log (count);
+    count+=1;
+}
+
+console.log ("print out all number divisible by 5 and 3 between 5 and 50")
+var count= 5;
+while (count <=50){
+    if( count% 5===0 && count% 3===0 )
+    console.log (count);
+    count+=1;
+}
+
