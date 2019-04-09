d3f43fbfa191b9eb5f76afd177f7175e7c74f36d
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-10 01:10:54 +0600

    Add files via upload

---
+++ Introduction JS Function/style.js
@@ -0,0 +1,7 @@
+function singSong () {
+    console.log ("Twinkle, twinkle, little star,");
+    console.log ("How I wonder what you are!,");
+    console.log ("Up above the world so high,");
+    console.log ("Like a diamond in the sky.");
+}
+
