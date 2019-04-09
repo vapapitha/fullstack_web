fe6340c988d956a8af158dc3528cd7bab4c2ffef
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-10 04:40:54 +0600

    Update style.js

--- Return key/style.js
+++ Return key/style.js
@@ -1,3 +1,16 @@
 function square (num){
     console.log (num*num)  
   } 
+
+function square (x-){
+    console.log (x*x)  
+  } 
+
+
+  function test ( x,y){
+    return x-y;
+}
+
+function test ( x){
+  return x*2;
+}
