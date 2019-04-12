8dfa20743ad3a5802860a31cababd8327426741b
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-12 06:59:58 +0600

    Add files via upload

---
+++ odd and even number/script.js
@@ -0,0 +1,10 @@
+var num = prompt ("Input your number")
+
+if(num % 2 !== 0) {
+console.log("Your age is odd!");
+alert("Your age is odd!")
+}
+
+ if (num% 2 === 0){
+    console.log("Your age is even!");
+    alert("Your age is even!")}
