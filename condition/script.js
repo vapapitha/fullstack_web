288391c1aa13234524592fe4bf95c0631121c401
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-12 06:28:46 +0600

    Add files via upload

---
+++ condition/script.js
@@ -0,0 +1,36 @@
+var age = Number(prompt("What is your age?"));
+
+// If age is negative
+
+if (age<0){
+    console.log ("Come back once you're out of the womb");
+    alert("Come back once you're out of the womb");
+    }
+
+//  if 
+if (age<21){
+    console.log ("Come back once you're out of the womb");
+    alert("Apnar akhono biyer boyosh hoy nai ");
+    }
+
+    // If age is 21
+    if (age===21){
+        console.log ("Come back once you're out of the womb");
+        alert("Akhon apnar biyer boyosh hoiche");
+        }
+
+
+        // If age is between 21 and 30
+
+       if (21<age && age<30){
+            console.log ("Come back once you're out of the womb");
+            alert("akhon apnar biyer boyosh chole jacche");
+            }
+
+            // If age is over 30
+
+            if (age>30){
+                console.log ("Come back once you're out of the womb");
+                alert("Apni akhon biye koira ki korben???????????????");
+                }
+
