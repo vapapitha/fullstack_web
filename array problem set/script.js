1324a3255c3109ef130e11b85dbf6ee4c1c32c87
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-18 03:05:31 +0600

    Add files via upload

---
+++ array problem set/script.js
@@ -0,0 +1,9 @@
+// print Reverse()
+console.log ("connacted");
+function printReverse(arr){
+    for(var i= arr.lenght-1;i>=0;i-- ){
+        console.log( arr[i])
+
+    }
+}
+printReverse([1,2,9,5])
