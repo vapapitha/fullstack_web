82905f8d20882e5aec3fcf37d50325c83ecf0b98
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-20 03:41:21 +0600

    Add files via upload

---
+++ MANIPULATE/script.js
@@ -0,0 +1,16 @@
+
+// ADD a class to the selected element
+var p=document.querySelector("p")
+p.classList
+p.classList.add("big")
+
+
+// Remove  a class to the selected element
+var p=document.querySelector("p")
+p.classList
+p.classList.remove("big")
+
+// Toggle a class to the selected element
+var p=document.querySelector("p")
+p.classList
+p.classList.toggle("big")
