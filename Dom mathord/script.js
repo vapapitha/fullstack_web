c73333281a10da9adaec5b235f983b01a93251ad
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-19 06:43:22 +0600

    Add files via upload

---
+++ Dom mathord/script.js
@@ -0,0 +1,24 @@
+//  document.getElementById("highlight")
+
+var tag = document.getElementById("highlight")
+
+
+// document.getElementsByClassName()
+
+var tags = document.getElementsByClassName("bolded")
+
+// document.getElementsByTagName()
+
+var tags = 
+    document.getElementsByTagName("li")
+
+// document.querySelector()
+
+var tags = 
+    document.querySelector("#highlight")
+
+
+// document.querySelectorAll()
+
+var tags = 
+    document.querySelectorAll(".bolded")
