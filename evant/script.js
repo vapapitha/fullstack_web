52102a46acf74a5c078e60eeb5aa22b192ecd89a
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-23 22:12:38 +0600

    Add files via upload

---
+++ evant/script.js
@@ -0,0 +1,8 @@
+var h1= document.querySelector("H1");   
+h1.addEventListener("click",function() {
+   h1.style.background = "blue";
+})
+var ul= document.querySelector("Ul").addEventListener("click",function(){
+    console.log("CLICK")
+    });
+    var lis= document.querySelectorAll("li"); 
