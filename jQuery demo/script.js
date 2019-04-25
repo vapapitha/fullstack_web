eaa3f620c140ce18e445a6dc05608235a3e23053
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-26 02:04:28 +0600

    Add files via upload

---
+++ jQuery demo/script.js
@@ -0,0 +1,11 @@
+$("h1").css("color", "green");
+
+var styles= {
+    color:("blue"),
+    background:("yellow"),
+    border:("2px solid purple"),
+    }
+    $("h1").css(styles);
+
+    $("li").css("color", "red");
+    $("a").css("font-size", "40px");
