176c3cff92ffd1e9c66e824fdd030f69db088a27
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-27 23:21:03 +0600

    Add files via upload

---
+++ just fun with js/script.js
@@ -0,0 +1,4 @@
+var  firstName=prompt("What is your first Name???????")
+var  lastName=prompt("What is your last Name???????")
+var age=prompt("What is your age???????")
+alert("hello!!! "+ firstName +" " + lastName +","+ " you are " + age + " years old.....")
