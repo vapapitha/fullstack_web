0d7817b93665baa79eada09157be2bf96af789d4
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-04-30 04:16:50 +0600

    Add files via upload

---
+++ jQuery event/script.js
@@ -0,0 +1,10 @@
+// click
+$("h1").click(function(){
+    alert("h1 clicked!!!")
+    });
+
+    keypress
+    $("input")
+    $("input").keypress(function(){
+        console.log("You pressed a key!!!")
+        }) 
