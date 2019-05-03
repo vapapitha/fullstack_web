e2895ef1859d6eeae8098a2fb9e37f76b1979fca
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-04 01:25:07 +0600

    Add files via upload

---
+++ math solve by for loop/script.js
@@ -0,0 +1,48 @@
+
+// print out all number between -10 and 19
+
+for (var i =-10;i < 19;i++){
+    console.log (i)
+    }
+
+
+// print out all even number between -10 and 19
+
+    for (i = 10;i < 40;i++){
+        if (i % 2 === 0){
+        console.log(i)
+        }
+    }
+
+// print out all odd number between -10 and 19
+
+    for (i = 10;i < 40;i++){
+        if (i%2 !== 0){
+        console.log(i)
+        }
+    }
+
+
+
+    for (i = 5;i<50;i++){
+        if(i % 3 === 0 && i % 5=== 0){
+            console.log(i)
+        }
+    }
+
+// print out all which not divisiable by 3 and 5 number between -10 and 19
+
+    for (i = 5;i < 50;i ++){
+        if(i % 3 !== 0 && i%5 !== 0){
+            console.log(i)
+        }
+    }
+
+
+// print out all  divisiable number between -10 and 19
+
+    for (i = 5;i < 50;i++){
+        if(i % 3 === 0 || i % 5 ===0){
+            console.log(i)
+        }
+    }
