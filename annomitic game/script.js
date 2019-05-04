69c700847fa9009cf633d54bf82a69089e5bf95a
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-04 06:31:46 +0600

    Add files via upload

---
+++ annomitic game/script.js
@@ -0,0 +1,28 @@
+var answer = prompt ("are  we there yet?");
+
+while (answer !== "yes" ){
+    
+    var answer = prompt ("are  we there yet?");
+}
+
+if(answer === "yes"){
+    alert("YAY!!we made it...")
+}
+
+
+
+
+
+
+
+
+
+
+// var answer = prompt ("are  we there yet?");
+
+// while (answer !== "yes" ){
+//     if(answer === "yes"){
+//         alert("YAY!!we made it...");
+//     }
+//     var answer = prompt ("are  we there yet?");
+// }
