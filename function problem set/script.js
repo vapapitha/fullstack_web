0c821f71dc3ade4f6b89c4b2e93dc254a7de9d5e
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-08 03:17:10 +0600

    Add files via upload

---
+++ function problem set/script.js
@@ -0,0 +1,40 @@
+
+
+function isEven (num) {
+    if (num % 2 === 0 ){
+        return true;
+    }   
+    else {
+        return false;
+    }
+}
+
+
+//  function factorial (num){
+//    var result = 1;
+//   for (var i = 2; i <= num; i++){
+//     result = result * i
+//      // result *= i;
+//    }
+//    return result;
+// }
+ 
+
+// factorial number
+
+// function factorial (num){
+//     var result = 1;
+//     for (var i = 2; i <= num; i++){
+//       result = result * i
+//       // result *= i;
+//     }
+//     return result;
+//}
+
+// var num = Number(prompt ())
+// if (num % 2 === 0 ){
+//     alert ("true");
+// }   
+// else {
+//     alert ("false");
+// }
