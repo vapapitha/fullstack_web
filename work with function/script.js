631dbacfc9fe753c4377a5a9f02a3a312f67af21
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-10 05:18:53 +0600

    Add files via upload

---
+++ work with function/script.js
@@ -0,0 +1,78 @@
+// // practice 1
+
+// function declaration 
+
+
+// function sayHell (){
+
+//     alert ("hello");
+//     console.log("hello")
+// }
+// sayHell ()
+
+
+
+
+// // practice 2
+
+// function expression
+
+// var sayBye = function  (){
+    
+//     alert ("bye");
+      
+//    console.log("bye")
+//      }
+
+
+//      sayBye ();
+
+
+
+// // practice 3
+
+
+// function multiply (a,b){
+
+//     return a*b 
+//  }
+
+ // // practice 4
+
+
+//  function multiply (a,b){
+
+//     return a;
+//     return a*b ;
+//     return b;
+//  }
+
+// // NB: only return  a work
+
+
+// // practice 5
+
+// function multiply (a,b){
+//     if (a>10||b>10){
+//         return "It works fine"
+//     }
+//     else {
+//         return  a * b
+//     }
+//  }
+
+
+ // practice 5
+// NB:function work as variable 
+
+
+
+
+var a = function  (a,b){
+    if (a>10||b>10){
+        return "It works fine"
+    }
+    else {
+        return  a * b
+    }
+ }
