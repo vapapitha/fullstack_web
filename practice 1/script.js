696117c749fbc817b2c2e0642bcc7aac74548ba4
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-11 00:22:55 +0600

    Add files via upload

---
+++ practice 1/script.js
@@ -0,0 +1,28 @@
+// print all number from 40 to 1000 by while loop
+
+
+// var count = 40 ;
+// while (count<=1000){
+//     console.log (count);count++
+
+// }
+
+// print all number from 40 to 1000 by for loop
+
+
+// for (i=40; i<100; i++){
+//     console.log (i);
+// }
+
+// find out odd and even number 
+
+
+// var number = Number (prompt ("inter a number"));
+
+// if (number % 2 === 0 ){
+//     alert ("this is a even number")
+// }
+// else if (number % 2 !== 0) {
+//     alert ("this is a odd number")
+// }
+// else { alert("plz input a number")}
