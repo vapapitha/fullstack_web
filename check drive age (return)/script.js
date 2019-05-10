318659e7bf003c83581ea5921b05a4f7b5cd4aaf
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-11 04:46:38 +0600

    Add files via upload

---
+++ check drive age (return)/script.js
@@ -0,0 +1,9 @@
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		return "Sorry, you are too young to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
