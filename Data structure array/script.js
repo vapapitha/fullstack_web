d9e095f604eeff325313c0c26ec9f166a4dd3b13
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-14 04:49:00 +0600

    Add files via upload

---
+++ Data structure array/script.js
@@ -0,0 +1,14 @@
+var list = ["noman","rony","jony","manik","kawsar"]
+
+var list = [["noman","rony","jony","manik","kawsar"]]
+
+var list = ["noman","rony","jony","manik","kawsar"]
+
+list.shift ();
+list.pop ();
+list.push("jamal")
+list.concat(["jisadh","anik"])
+
+
+var newList = ["noman","rony","jony","manik","kawsar"]
+var anotherNewList = newList.concat(["jisadh","anik"])
