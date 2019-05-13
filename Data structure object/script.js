b2eb1b63e38c290c0a733ad55c159e066ee24a2e
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-14 04:49:40 +0600

    Add files via upload

---
+++ Data structure object/script.js
@@ -0,0 +1,12 @@
+var user  = {
+    name : "sakib",
+    age : 24,
+    hobby: "Football",
+    isMarried: false,
+
+}
+user.name
+user.age
+user.hobby
+user.favouritecolore = "green"
+user.isMarried = true;
