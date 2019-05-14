a518036bc829b12f0304e655ecb4f4577726512e
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-15 04:45:29 +0600

    Add files via upload

---
+++ loop/script.js
@@ -0,0 +1,52 @@
+var todos = [
+    "wake up early",
+    "clean room",
+    "brush teeth",
+    "eat health",
+    "study js",
+    
+    ];
+
+todos.forEach(function(i){
+    console.log(i)
+})
+
+// for (var i=0; i<todos.length; i++){
+//     console.log(todos[i])
+//     };
+
+
+
+    // for (var i=0; i<todos.length; i++){
+    //     console.log(todos[i] + "!")
+    //     };
+
+
+
+    // for (var i=0; i<todos.length; i++){
+    //     todos[i] = todos[i] + "!"
+    //     };
+
+
+
+    // var todosLength =todos.length
+
+    // for (var i=0; i<todosLength; i++){
+    //     todos.pop();
+    //     };
+
+
+
+// var count = 0
+
+// while (count < 10){
+//     console.log (count);count++
+
+// }
+
+// var count = 10
+
+// while (count > 0){
+//     console.log (count);count--
+
+// }
