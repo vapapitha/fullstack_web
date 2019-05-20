57f92e1f1b09a72fa174a8692aa90dd490922003
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-21 05:20:41 +0600

    Add files via upload

---
+++ practice 3/script.js
@@ -0,0 +1,37 @@
+var date = prompt("what is day today??? ")
+
+var sunday = "Sunday"
+var monday = "Monday"
+var tuesday = "Tuesday"
+var wednesday = "Wednesday"
+var thursday = "Thursday"
+var friday = "Friday"
+var saturday = "Saturday"
+
+
+
+
+if (date === sunday){
+   alert("Only OVI is active")
+ }
+ else if (date === monday){
+    alert("Only AYAN is offline")
+  }
+  else if (date === tuesday){
+    alert("ALL are active")
+  }
+  else if (date === wednesday){
+    alert("ALL are active")
+  }
+  else if (date === thursday){
+    alert("ALL are active")
+  }
+  else if (date === friday){
+    alert("Only OVI is offline")
+  }
+  else if (date === saturday){
+    alert("Only AYAN is active")
+  }
+  else { alert("plz input date like Sunday,Monday.......")}
+
+  
