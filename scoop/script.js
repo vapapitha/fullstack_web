e6304a93ec4255a55cf53bda860e95829d6b4073
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-29 07:28:36 +0600

    Add files via upload

---
+++ scoop/script.js
@@ -0,0 +1,29 @@
+// // Root scope (window)
+// var fun = 5;
+// function funfunction(){
+//     // Child scope
+//     var fun = (1,mhello)
+
+//     console.log(fun)
+// } 
+
+// function funerfunction(){
+//     // Child scope
+//     var fun = (byeeeeeeeeeeeeeeeee);
+
+//     console.log(2,fun)
+// } 
+
+// function funestfunction(){
+//     // Child scope
+//     var fun = (tata)
+
+//     console.fun(3,fun)
+// } 
+
+// console.log("windos",fun)
+
+
+
+
+
