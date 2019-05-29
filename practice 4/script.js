3327d79a1cccabd4b5e5060c3e38673b26427ccd
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-29 07:27:55 +0600

    Add files via upload

---
+++ practice 4/script.js
@@ -0,0 +1,26 @@
+var counter = 0
+
+
+for( var i = 5;i <= 50;i++){
+    if (i % 3 === 0 && i % 5 === 0 ){
+        counter+=i
+    }
+    console.log(counter)
+}
+
+
+
+for( var i=5;i <50;i++){
+    if ( i % 3 === 0 && i % 5 === 0 ){
+        console.log(i);
+     }
+        
+}
+
+
+
+
+
+for(var i=5;i <= 50;i++){
+    console.log(i);
+}
