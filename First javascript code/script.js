1f8d580f104b3c04a4a59594843fb719b2bfa5bc
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-05-30 06:20:48 +0600

    Add files via upload

---
+++ First javascript code/script.js
@@ -0,0 +1,23 @@
+// var price = 28.99;
+// var discount = 10;
+// var total = (price *(discount/100));
+// if (total > 25){
+//     freeShipib
+
+// var count = 10;
+// while (count > 0){
+//     juggle();
+//     Count = COUNT-1
+// }
+
+//var dog = ( name:"Rover",weight:"35");
+// if (dog.weight > 30){
+//     alert("WOOF WOOF")
+// } else {
+//     alert("woof woof")
+// }
+
+
+// var circleRadius = 20;
+// var circleArea = 
+// Math.ip * (circleRadius * circleRadius);
---
+++ Introduction/script.js
@@ -0,0 +1,4 @@
+setTimeout (wakeUpUser,1000)
+function wakeUpUser(){
+    alert("Are you going to stare at boring page forever?")
+}
