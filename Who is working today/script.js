e85cf2f4a8972e47e0b011357557c5676aaf2fbc
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-11 01:36:04 +0600

    Add files via upload

---
+++ Who is working today/script.js
@@ -0,0 +1,20 @@
+// var button = document.all[8].addEventListener("click",buttonclick())
+
+function buttonclic1(){
+    alert("Only Ovi is working today.(9pm to 5am)")
+};
+function buttonclic2(){
+    alert("Only Ayan is working today.(9pm to 5am)")
+};
+function buttonclic3(){
+    alert("Sadman,Ismail,Tan is working from 9pm to 5am and Ovi working from 12pm to 8am)")
+};
+function buttonclic4(){
+    alert("Sadman,Ismail,Tan is working from 9pm to 5am and Ayan  working from 12pm to 8am)")
+};
+function buttonclic5(){
+    alert("Sadman,Ismail,Tan is working from 9pm to 5am and Ayan,Ovi  working from 12pm to 8am)")
+};
+function buttonclic1(){
+    alert("Only Ovi is working today.(9pm to 5am)")
+}
