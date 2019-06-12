9b56a8763c371e2337a31a9ae53ed14f94518ba8
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-13 01:31:25 +0600

    Update script.js

--- shopping list (uncomplete)/script.js
+++ shopping list (uncomplete)/script.js
@@ -1,14 +1,12 @@
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
-// Filter event
-filter.addEventListener('keyup', filterItems);
+
 
 // Add item
 function addItem(e){
