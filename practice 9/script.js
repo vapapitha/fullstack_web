0ce5757e84b0040d5246ff506b52a06605c826ad
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-18 01:29:25 +0600

    Add files via upload

---
+++ practice 9/script.js
@@ -0,0 +1,46 @@
+// printing all number between -10 and 19"
+
+for (i = -10; i <= 19; i++) { 
+    console.log(i);
+  }
+
+
+
+//   printing all number between 10 and 40"
+
+
+for (i = 10; i <= 40; i++){
+    console.log(i)
+}
+
+
+// ("print out all even number between 300 and 333")
+
+for(i = 300; i <= 333; i++){
+    if(i%2 == 0){
+        console.log(i)
+    }
+}
+
+// ("print out all odd number between 300 and 333")
+
+for(i = 300; i <= 333; i++){
+    if(i%2 !== 0){
+        console.log(i)
+    }
+}
+
+// console.log ("print out all number divisible by 5 and 3 between 5 and 50")
+
+
+
+for (i = 5; i<= 50; i++){
+    if(i% 5===0 && i% 3===0 ){
+        console.log(i)
+    }
+}
+
+
+
+
+
