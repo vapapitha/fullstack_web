e52b3c05e4c5d325c4e6309f0424a9f1be185a3e
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-19 00:30:43 +0600

    Add files via upload

---
+++ Date & time/script.js
@@ -0,0 +1,25 @@
+var today = new Date();
+  
+ 
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  
+
+  var millisecond = today.getMilliseconds();
+
+  
+
+
+function date(){
+ 
+alert("Today is : " + today+ ".");
+  }
+
+
+  
+function time(){
+ 
+  alert(" Time is (Hour : Minute : Second : Millisecond ) : " + hour + " : " + minute + " : " + second  + " : " + millisecond );
+    }
+  
