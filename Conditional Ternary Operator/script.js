291f031c9d6d8fa285ea2cc0d4e18e2143d7d0e5
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-19 02:23:50 +0600

    Add files via upload

---
+++ Conditional Ternary Operator/script.js
@@ -0,0 +1,8 @@
+
+        function myFunction() {
+          var age, voteable;
+          age = document.getElementById("age").value;
+          voteable = (age <= 18) ? "Too young":"Old enough";
+          document.getElementById("demo").innerHTML = voteable + " to vote.";
+        }
+    
