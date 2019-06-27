828374f1e295514915b9d76abfdf0dc83c563792
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-28 02:38:38 +0600

    Add files via upload

---
+++ practice problem solve 06/script.js
@@ -0,0 +1,24 @@
+// kisu somossa ase.....if statment keno emon???????????
+
+
+
+
+
+
+function difference(n)
+ {
+//    return (n > 13)?(n* 3):(13 - n); 
+
+    if (n > 13){
+        return n* 3;
+    }
+
+    
+else{
+    return 13 - n;
+}
+    
+
+ }
+
+ console.log(difference(61))
