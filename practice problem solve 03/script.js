393fac6da20d318f2e6bd02c5fa40864e80780b9
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-27 21:49:38 +0600

    Add files via upload

---
+++ practice problem solve 03/script.js
@@ -0,0 +1,11 @@
+var side1 = 5; 
+var side2 = 6; 
+var side3 = 7; 
+
+var s = (side1 + side2 + side3) / 2
+var a = Math.sqrt(s* (s - side1 )*(s - side2)*(s - side3))
+alert(a);
+
+
+// var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+// alert(area);
