33bcacb9093f618ced77b988e43268fad84e7cb4
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-06-27 21:49:56 +0600

    Add files via upload

---
+++ practice problem solve 05/script.js
@@ -0,0 +1,20 @@
+
+var year = Number(prompt("enter a Year"))
+
+year = (year % 4 == 0)?"It is a leap year":"It is not leap year"; 
+
+alert(year)
+
+
+
+
+
+
+
+// var year = Number(prompt ("enter a Year"));
+// if(year%4==0){
+//     alert("It is a leap year")
+// }
+// else if(year % 4 !== 0){
+//     alert("It is a not leap year")
+// }
