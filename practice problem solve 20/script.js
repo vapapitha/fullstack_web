c28e1f48db8cc2cc5ad9137dee8a9222a13f74be
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-02 00:18:59 +0600

    Update script.js

--- practice problem solve 20/script.js
+++ practice problem solve 20/script.js
@@ -1,4 +1,16 @@
-function print_current_page()
+function last_digit(x, y, z)
 {
-window.print();
+  if ((x > 0) && (y > 0) && (z > 0))
+    {
+     return ( (x % 10 == y % 10) && (y % 10 == z % 10) && (x % 10 == z % 10));
+   }
+  else
+    return false;
 }
+
+console.log(last_digit(20, 30, 400));
+console.log(last_digit(24, 30, 400));
+console.log(last_digit(20, 36, 400));
+console.log(last_digit(25, 35, 405));
+
+
