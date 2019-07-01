dc882b010830d0ad8f12aaa6ce358339ae7afef8
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-02 00:49:56 +0600

    Add files via upload

---
+++ practice problem solve 22/script.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to compute the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+
+
+function sum (x,y) {
+ 
+  if (((x+y)<80) && ((x+y)>60)){
+    return 65;
+  }else{
+    return 80;
+  }
+  
+  
+ }
+
+
+
