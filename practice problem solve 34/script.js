209eb4d40276f509a1ee51c3733d334360bea409
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-04 04:18:46 +0600

    Add files via upload

---
+++ practice problem solve 34/script.js
@@ -0,0 +1,19 @@
+
+// Write a JavaScript program to check three given numbers, if the three numbers are same return 30 otherwise return 20 and if two numbers are same return 40
+
+// Write a JavaScript program to convert the letters of a given string in alphabetical order.
+
+
+
+function test(str) { 
+
+  return str.split("").sort().join("");
+       
+}
+
+console.log(test("hello"));
+console.log(test("welcome"));
+
+
+
+
