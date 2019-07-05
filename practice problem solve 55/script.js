c3fb5f0f1ce40ece1bd969a1df8ef4a531c2faa7
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-06 03:14:11 +0600

    Add files via upload

---
+++ practice problem solve 55/script.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to extract the first half of a string of even length
+function first_half (str) {
+  if (str.length % 2 == 0) {
+    return str.slice(0, str.length / 2);
+  }else{
+    return str;
+  }
+  
+}
+console.log(first_half("Python"));  
+console.log(first_half("JavaScript")); 
+console.log(first_half("PHP"));
