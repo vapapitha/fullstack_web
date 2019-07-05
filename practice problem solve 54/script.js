9bd158f366fd810edcdf9b2e59b520391872f538
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-06 03:13:34 +0600

    Add files via upload

---
+++ practice problem solve 54/script.js
@@ -0,0 +1,16 @@
+//Write a JavaScript program to create a new string of 4 copies of the last 3 characters of a given original string. The length of the given string must be 3 and above
+
+
+
+function newstring(str)
+{
+  if (str.length >= 3) {
+    result_str = str.substring(str.length - 3);
+    return result_str + result_str + result_str + result_str;
+  }
+  else
+    return false;
+}
+console.log(newstring("Python 3.0"));
+console.log(newstring("JS"));
+console.log(newstring("JavaScript"));
