2168218669ccf8be120d7e078cd7f1f14c1a4f03
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-06 03:11:21 +0600

    Add files via upload

---
+++ practice problem solve 52/script.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to convert the letters of a given string in alphabetical order.
+
+function alphabet_Soup(str) { 
+
+  return str.split("").sort().join("");
+       
+}
+
+console.log(alphabet_Soup("Python"));
+console.log(alphabet_Soup("Welcome"));
+console.log(alphabet_Soup("Exercises"));
