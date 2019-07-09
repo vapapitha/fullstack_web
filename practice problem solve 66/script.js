f76217a0b037ef987fafe432c8b355ad5bc885f7
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-10 01:55:41 +0600

    Add files via upload

---
+++ practice problem solve 66/script.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to find the larger value between the first or last and set all the other elements with that value. Display the new array.
+
+
+function all_max(nums) 
+ {
+    var max_val = nums[0] > nums[2] ? nums[0] : nums[2];
+
+    nums[0] = max_val;
+    nums[1] = max_val;
+    nums[2] = max_val;
+
+    return nums;
+}
+console.log(all_max([20, 30, 40]));
+console.log(all_max([-7, -9, 0]));
+console.log(all_max([12, 10, 3]));
