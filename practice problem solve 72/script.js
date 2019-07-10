3eb1f34c0fce96c14e5c80dd00f51539af1ed7d6
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-11 04:16:48 +0600

    Add files via upload

---
+++ practice problem solve 72/script.js
@@ -0,0 +1,23 @@
+function check(x,y,z) {
+
+  if ((x>y)&& (x>z)){
+    console.log(x)
+  }
+
+  else if ((z>y)&& (z>y)){
+    console.log(z)
+  }
+
+
+  else{
+    console.log(y)
+  }
+
+
+  }
+
+
+check(89, 63,29);
+check(44, 56 , 10);
+check(70, 48, 95);
+
