53979fba584187c48cbfacbefa06ee67bdeed342
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-07-12 04:24:55 +0600

    Add files via upload

---
+++ practice problem solve 78/script.js
@@ -0,0 +1,11 @@
+
+// Write a JavaScript program to check if the characters a and b are separated by exactly 3 places anywhere (at least once) in a given string.
+
+function ab_Check(str)
+ {
+    return (/a...b/).test(str) || (/b...a/).test(str);
+ }
+
+console.log(ab_Check("Chainsbreak"));
+console.log(ab_Check("pane borrowed"));
+console.log(ab_Check("abCheck"));
