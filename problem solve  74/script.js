dd31d6e411523206fb5b4b646e7fb902f6a16c45
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-03 04:11:54 +0600

    Add files via upload

---
+++ problem solve  74/script.js
@@ -0,0 +1,13 @@
+function all_max(nums) 
+ {
+    var max_val = nums[0] > nums[2] ? nums[0] : nums[2];
+
+    nums[0] = max_val;
+    nums[1] = max_val;
+    nums[2] = max_val;
+
+    return nums;
+}
+console.log(all_max([20, 30, 40]));
+console.log(all_max([-7, -9, 0]));
+console.log(all_max([12, 10, 3]));
