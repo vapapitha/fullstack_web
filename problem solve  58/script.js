a7bd58bed6a6022f5094e3d668cf362e1979c09b
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-07 06:19:51 +0600

    Add files via upload

---
+++ problem solve  58/script.js
@@ -0,0 +1,12 @@
+function newstring(str)
+{
+  if (str.length >= 3) {
+    result_str = str.substring(str.length - 3);
+    return result_str + result_str + result_str + result_str;
+  }
+  else
+    return false;
+}
+console.log(newstring("Python 3.0"));
+console.log(newstring("JS"));
+console.log(newstring("JavaScript"));
