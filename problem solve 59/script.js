e77e2e01dc697e9c901e84e31c43925c3badd49a
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-07 06:20:16 +0600

    Add files via upload

---
+++ problem solve 59/script.js
@@ -0,0 +1,10 @@
+function first_half (str) {
+    if (str.length % 2 == 0) {
+      return str.slice(0, str.length / 2);
+    }
+    return str;
+  }
+  console.log(first_half("Python"));  
+  console.log(first_half("JavaScript")); 
+  console.log(first_half("PHP"));
+  
