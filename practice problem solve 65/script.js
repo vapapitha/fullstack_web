56c3d1fd50457e8baa256f6233982288415be341
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-08 07:13:28 +0600

    Add files via upload

--- practice problem solve 65/script.js
+++ practice problem solve 65/script.js
@@ -1,13 +1,11 @@
-// Write a JavaScript program to check if 1 appears in first or last position of a given array of integers. The array length must be greater or equal to 1.
-
-
-function first_last_1(nums)
-{
-  var end_pos = nums.length - 1;
-  return nums[0] == 1 || nums[end_pos] == 1;
+function script(str) {
+  if (str.substring(str.length - 6, str.length) == 'Script') 
+  {
+    return true;
+  } 
+  else 
+  {
+    return false;
+  }
 }
-
-
-console.log(first_last_1([1, 3, 5]));
-console.log(first_last_1([1, 3, 5, 1]));
-console.log(first_last_1([2, 4, 6]));
+console.log(script("JavaScript"));
