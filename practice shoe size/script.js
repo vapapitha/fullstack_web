61d3d516ab86c21c745714ef680c38505b68a120
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-11 01:23:38 +0600

    Add files via upload

---
+++ practice shoe size/script.js
@@ -0,0 +1,12 @@
+var size=prompt(" What is your shoe size????")
+var multiply=(size * 5);
+var add= (multiply+ 50);
+var totalMultiple=(add*20);
+var againAdd=(totalMultiple + 1019);
+var subtract=(againAdd - prompt("Subtract the year you were Born.."))
+alert( "Last two numbers are your AGE and  first other number is your shoe SIZE  " + subtract);
+
+
+// if (size<=9 && subtract>=2009){
+//     alert("" +  subtract)}
+
