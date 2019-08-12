139a9fa37c336e5d8b9c60ea9d11aa7e1311b68c
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-12 23:28:43 +0600

    Add files via upload

--- Guessing game/script.js
+++ Guessing game/script.js
@@ -1,19 +1,19 @@
 // create secretNunber
 var secretNumber = 5;
 //  ask for guessing number
-var guess = prompt ("Guess your Number")
+var guess = prompt ("Guess your Number");
 // check number if it is right
 if (Number (guess)=== secretNumber){
-    alert ("Your answer is correct")
+    alert ("Your answer is correct");
 }
 // check if is too high
 
 else if ( Number (guess)> secretNumber) {
-    alert ("Your answer is too high")
+    alert ("Your answer is too high");
 }
 
 // check if is too low
 
 else if ( Number (guess)< secretNumber) {
-    alert ("Your answer is too low")
+    alert ("Your answer is too low");
 }
