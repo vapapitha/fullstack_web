9ba5400abd3592fe2df66cc042b2f36bd1a30dc0
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-08-17 21:43:35 +0600

    Add files via upload

---
+++ Practice DOM 2/script.js
@@ -0,0 +1,20 @@
+
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px #000';
+
+
+
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
