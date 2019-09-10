be52b7521908e50743cffb342e0ea117c9cf32e4
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-09-10 21:17:26 +0600

    Add files via upload

---
+++ jQuery 1/script.js
@@ -0,0 +1,15 @@
+$("h1")
+
+$ ("a")
+
+$("h1").css("color", "green");
+
+var styles= {
+    color:("blue"),
+    background:("yellow"),
+    border:("2px solid purple"),
+    }
+    $("h1").css(styles);
+
+    $("li").css("color", "red");
+    $("a").css("font-size", "40px");
