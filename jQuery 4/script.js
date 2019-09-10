68bc737f5ce7a014b473a751659f9a3086c1d00a
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-09-10 21:19:39 +0600

    Add files via upload

---
+++ jQuery 4/script.js
@@ -0,0 +1,20 @@
+// .text
+$("h1")
+$("h1").text(  )
+$("h1").text( "I want to change h1" )
+// $("li").text(  )
+
+
+// ,html
+$("ul")
+$("ul").html(  )
+$("ul").html( "<li> kalo</li><li> lal</li><li> nil</li>" )
+
+// attr
+$("img").css("width")
+$("img").css("width","300px")
+$("img").css("height")
+$("img").css("height","300px")
+$("img").attr ("src","/home/rifat/Documents/jQuery/jQuery methord/art-ball-ball-shaped-235615.jpg")
+$("input").attr ("type")
+$("input").attr ("type","color") 
