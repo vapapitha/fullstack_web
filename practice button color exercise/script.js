b809aecaca5531facb5850cb220c7a65e64845db
Sakibur Rahman <46139077+vapapitha@users.noreply.github.com>
2019-09-13 23:07:47 +0600

    Add files via upload

---
+++ practice button color exercise/script.js
@@ -0,0 +1,11 @@
+var button= document.querySelector("Button"); 
+
+var paragraph= document.querySelector("p");
+
+button.addEventListener("click",function() {
+    paragraph.textContent = "welcome world";});
+
+
+    button.addEventListener("click",function() {
+        button.style.background = "green";
+     })
